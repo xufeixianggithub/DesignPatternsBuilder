1c30b30822e3fa63a17528fab47600e1fbee6c76
zude <zude@seeapp.com>
2018-08-16 11:02:38 +0800

    Initial commit

---
+++ src/main/java/com/xufx/ConcreteBuilder/ConcreteBuilder.java
@@ -0,0 +1,24 @@
+package com.xufx.ConcreteBuilder;
+
+import com.xufx.Builder.Builder;
+import com.xufx.Product.Product;
+
+/**
+ * 具体的构建器实现对象
+ */
+public class ConcreteBuilder implements Builder {
+    /**
+     * 构建器最终构建的产品对象
+     */
+    private Product resultProduct;
+    /**
+     * 获取构建器最终构建的产品对象
+     * @return 构建器最终构建的产品对象
+     */
+    public Product getResult() {
+        return resultProduct;
+    }
+    public void buildPart() {
+        //构建某个部件的功能处理
+    }
+}
---
+++ src/main/java/com/xufx/Product/Product.java
@@ -0,0 +1,8 @@
+package com.xufx.Product;
+
+/**
+ * 被构建的产品对象的接口
+ */
+public interface Product {
+    //定义产品的操作
+}
