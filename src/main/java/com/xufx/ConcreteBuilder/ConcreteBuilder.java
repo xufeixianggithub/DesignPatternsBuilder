d58421bd734bacf4f4cc4fa7d679c9176ab28123
zude <zude@seeapp.com>
2018-08-16 14:31:30 +0800

    创建者模式 创建合同示例

--- src/main/java/com/xufx/ConcreteBuilder/ConcreteBuilder.java
+++ src/main/java/com/xufx/ConcreteBuilder/ConcreteBuilder.java
@@ -1,24 +1,82 @@
 package com.xufx.ConcreteBuilder;
 
-import com.xufx.Builder.Builder;
-import com.xufx.Product.Product;
-
 /**
- * 具体的构建器实现对象
+ * 构造保险合同对象的构建器
  */
-public class ConcreteBuilder implements Builder {
+public class ConcreteBuilder {
+    private String contractId;
+    private String personName;
+    private String companyName;
+    private long beginDate;
+    private long endDate;
+    private String otherData;
     /**
-     * 构建器最终构建的产品对象
+     * 构造方法，传入必须要有的参数
+     * @param contractId 保险合同编号
+     * @param beginDate 保险开始生效的日期
+     * @param endDate 保险失效的日期
      */
-    private Product resultProduct;
-    /**
-     * 获取构建器最终构建的产品对象
-     * @return 构建器最终构建的产品对象
-     */
-    public Product getResult() {
-        return resultProduct;
+    public ConcreteBuilder(String contractId,long beginDate,long endDate){
+        this.contractId = contractId;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
     }
-    public void buildPart() {
-        //构建某个部件的功能处理
+    /**
+     * 选填数据，被保险人员的名称
+     * @param personName  被保险人员的名称
+     * @return 构建器对象
+     */
+    public ConcreteBuilder setPersonName(String personName){
+        this.personName = personName;
+        return this;
+    }
+    /**
+     *  选填数据，被保险公司的名称
+     * @param companyName 被保险公司的名称
+     * @return 构建器对象
+     */
+    public ConcreteBuilder setCompanyName(String companyName){
+        this.companyName = companyName;
+        return this;
+    }
+    /**
+     * 选填数据，其它数据
+     * @param otherData 其它数据
+     * @return 构建器对象
+     */
+    public ConcreteBuilder setOtherData(String otherData){
+        this.otherData = otherData;
+        return this;
+    }
+    /**
+     * 构建真正的对象并返回
+     * @return 构建的保险合同的对象
+     */
+    public InsuranceContract build(){
+        return new InsuranceContract(this);
+    }
+
+    public String getContractId() {
+        return contractId;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public long getBeginDate() {
+        return beginDate;
+    }
+
+    public long getEndDate() {
+        return endDate;
+    }
+
+    public String getOtherData() {
+        return otherData;
     }
 }
--- src/main/java/com/xufx/Product/Product.java
+++
@@ -1,8 +0,0 @@
-package com.xufx.Product;
-
-/**
- * 被构建的产品对象的接口
- */
-public interface Product {
-    //定义产品的操作
-}
